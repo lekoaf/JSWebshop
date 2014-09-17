553248724c8547d8d7ca59c82280e8e8e9379471
martin <martin.troback@outlook.com>
2014-09-17 13:40:28 +0200

    Update to validation

--- js/formvalidate.js
+++ js/formvalidate.js
@@ -1,6 +1,10 @@
 function validate(formSelector){
 
 	var validators = {
+		map:{
+			" ": "",
+			"-": ""
+		},
 		string: function(element){
 			var val = element.val();
 			return (val && typeof val == 'string' && val.length > 0); 
@@ -12,6 +16,9 @@ function validate(formSelector){
 		},
 		number: function(element){
 			var val = element.val();
+			for (prop in this.map){
+				val = val.split(prop).join(this.map[prop]);
+			}
 			return (val && !isNaN(val) && val.length > 0);
 		}
 	};
@@ -27,14 +34,15 @@ function validate(formSelector){
 		var isValid = validators[validator](element);
 
 		if (!isValid){
-			element.addClass('validation-failed');
+			element.parent().parent().addClass('has-success');
 			element.parent().parent().addClass('has-error');
 		}
 		else if((++numberOfCorrectValidations) == inputs.length){
-			$(formSelector).submit();
+			// $(formSelector).submit();
+			console.log("Form success");
 		}
 		else{
-			element.addClass('validation-failed');
+			element.parent().parent().removeClass('has-error');
 			element.parent().parent().addClass('has-success');
 		}
 
