3d90fc69658d9e16650131ab180b15e44872ff53
Martin Trobäck <martintk@axis.com>
2015-06-29 13:08:11 +0200

    Hard coding a default category is a bad idea

--- public/app/controllers/itemController.js
+++ public/app/controllers/itemController.js
@@ -6,18 +6,18 @@
 
 		$scope.switchCategory = function(category){
 
-			// Set default category or switch to input category
-			category = category || 'kitchen';
-
 			$http.get('../db/items.json').success(function (items){
 				$log.log(items);
-				$scope.items = items[category];
 
 				angular.forEach(items, function (val, key){
 					$log.log(key);
 					$scope.categories.push(key);
 					$scope.$emit('categories', $scope.categories);
 				});
+			
+			// Set default category or switch to input category
+			category = category || $scope.categories[0];
+			$scope.items = items[category];
 
 			}).error(function (data, status, header, config){
 				$log.warn(data);
